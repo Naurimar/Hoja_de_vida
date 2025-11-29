26fedcef609c992896640d9dbba7b9a48192a15e
Naurimar <herrerayanika1@gmail.com>
2025-11-28 23:15:10 -0500

    refactor: extracción de datos personales en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
 ﻿// Actualización para commit del App.jsx
 
+import {
+  headerData,
+  aboutData,
+  contactData,
+  skillsData,
+  projectsData
+} from "./data";
+
 import Header from './components/Header'
 import About, { Educacion } from "./components/About";
 import Skills from './components/Skills'
 import Projects, { Experiencia } from "./components/Projects";
 import Contact from './components/Contact'
 import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <Header
+        nombre={headerData.nombre}
+        cargo={headerData.cargo}
+        ciudad={headerData.ciudad}
+        contacto={headerData.contacto}
+      />
+
+      <About descripcion={aboutData.descripcion} />
+
+      <Skills lista={skillsData} />
+
+      <Projects proyectos={projectsData} />
+
+      <Contact
+        telefono={contactData.telefono}
+        email={contactData.email}
+        github={contactData.github}
+      />
+    </>
+  );
+}
 
 export default function App() {
   return (
